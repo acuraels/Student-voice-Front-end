cdfb56ce60732286876c5ef2d4ca871bf37ad3df
acuraels <daniil.rib@mail.ru>
2024-11-27 12:56:15 +0500

    form-page-footer-links

--- src/components/FormPage/FormPageFooter.jsx
+++ src/components/FormPage/FormPageFooter.jsx
@@ -13,12 +13,20 @@ const FormPageFooter = () => {
                     <li className="form-page-footer__list-contacts">
                         <p className="form-page-footer__heading">Контакты</p>
                         <ul>
-                            <li><a className='form-page-footer__heading-link' href="mailto:project.irit@urfu.ru">Написать нам</a></li>
+                            <li><a className='form-page-footer__heading-link' href="mailto:rtf@urfu.ru">Написать нам</a></li>
                             <li><p>Екатеринбург, ул. Мира 32</p></li>
                         </ul>
                     </li>
                     <li className="form-page-footer__list-social">
                         <p className="form-page-footer__heading">Мы в социальных сетях</p>
+                        <div className="form-page-footer__links">
+                            <a href="https://vk.com/irit_rtf_urfu" className="form-page-footer__link">
+                                <img className="form-page-footer__social-icon" src="/VK.svg" alt="VK" />
+                            </a>
+                            <a href="https://telegram.me/Iritatoday" className="form-page-footer__link">
+                                <img className="form-page-footer__social-icon" src="/TG.svg" alt="Telegram" />
+                            </a>
+                        </div>
                     </li>
                 </ul>
             </div>
